7accc6015ff16543a0a67f5f5940fdc8010ca814
acenturyandabit <steeven.liu2@gmail.com>
2020-09-18 11:03:50 +1000

    websocket gitlite update

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.listen(8080);
 
 app.use(cors());
 
+var app2 = express();
+app2.get("*", (req, res) => res.send(`<script>window.location.href="http://localhost:8080"</script>`));
+app2.listen(80);
 
 app.post("/saveme", (req, res) => {
     let safeF = String(req.query.f).replace(/\./g, "_");
@@ -85,12 +88,112 @@ wsServer = new WebSocketServer({
     httpServer: wshtserver
 });
 
+let gitdir = ""
+
 // WebSocket server
-let cons = {};
 wsServer.on('request', function (request) {
     let connection = request.accept(null, request.origin);
-    let id;
+    let saveF;
+    let data;
+    let localCopy;
+    let lus;
     connection.on('message', function (message) {
+        let response = JSON.parse(message.utf8Data);
+        console.log(message.utf8Data);
+        switch (response.op) {
+            case "push":
+                // try and find an entry with msg.id
+                saveF = String(response.id).replace(/\./g, "_");
+                console.log(saveF);
+                try {
+                    localCopy = JSON.parse(String(fs.readFileSync(private.baseGitLocation + "/" + saveF + ".json")));
+                } catch (e) {
+                    //save file does not exist, create one
+                    connection.send(JSON.stringify({
+                        op: "accept",
+                        _lu_: 0
+                    }));
+                    break;
+                }
+                let wasSent = false;
+                for (let i = 0; i < response._lu_.length; i++) {
+                    if (localCopy[response._lu_[i].id]._lu_ == response._lu_[i]._lu_) {
+                        // accept this
+                        connection.send(JSON.stringify({
+                            op: "accept",
+                            _lu_: localCopy[response._lu_[i].id]._lu_
+                        }));
+                        wasSent = true;
+                        break;
+                    }
+                }
+                if (!wasSent) {
+                    // something is probably wrong because thats a lot of unsents
+                    //oh well
+                    ws.send(JSON.stringify({
+                        op: "accept",
+                        _lu_: response._lu_[response._lu_.length]._lu_
+                    }));
+                }
+                break;
+            case "pull":
+                saveF = String(response.id).replace(/\./g, "_");
+                try {
+                    data = JSON.parse(String(fs.readFileSync(private.baseGitLocation + "/" + saveF + ".json")));
+                    lus = Object.entries(data).map((i) => ({ _lu_: i[1]._lu_, id: i[0] }));
+                    let pow2 = 0;
+                    lus = lus.sort((a, b) => a._lu_ - b._lu_).filter((i, ii) => {
+                        if (!(ii % (2 ** pow2)) || ii == lus.length - 1) {
+                            pow2++;
+                            return true;
+                        }
+                    });
+                    connection.send(JSON.stringify({
+                        id: response.id,
+                        op: "push",
+                        _lu_: lus
+                    }));
+                } catch (e) {
+                    console.log(e);
+                    //save file does not exist, create one
+                    connection.send(JSON.stringify({
+                        op: "reject",
+                    }));
+                    connection.close();
+                    break;
+                }
+                break;
+            case "accept":
+                // send over the data
+                let dataToSend = lus.filter(i => i._lu_ >= response._lu_).map(i => ({ id: i.id, data: data[i.id] }));
+                connection.send(JSON.stringify({
+                    op: "transfer",
+                    data: dataToSend
+                }));
+                connection.close();
+                break;
+            case "transfer":
+                if (!localCopy) {
+                    localCopy = {};
+                }
+                console.log(saveF);
+                for (let i of response.data) {
+                    if (!localCopy[i.id] || localCopy[i.id]._lu_ < i.data._lu_) localCopy[i.id] = i.data;
+                }
+                fs.writeFileSync(private.baseGitLocation + "/" + saveF + ".json", JSON.stringify(localCopy));
+                break;
+            case "reject":
+                connection.close();
+                break;
+        }
+
+
+
+
+
+
+
+        /*
         //first, which document is this?
         message = message.utf8Data;
         if (message[0] == "!") {
@@ -113,9 +216,9 @@ wsServer.on('request', function (request) {
             // recieve save request
             let save = JSON.parse(message);
             cons[id].nano.fire("newData", save);
-        }
+        }*/
     });
     connection.on("close", () => {
-        cons.splice(cons.indexOf(connection), 1);
+        //cons.splice(cons.indexOf(connection), 1);
     });
 });
