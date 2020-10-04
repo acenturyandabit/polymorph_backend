734f88392103e6eaa3a54b195ff00ff46fdc4ff1
acenturyandabit <steeven.liu2@gmail.com>
2020-10-05 10:55:47 +1100

    add private sample js

--- private_sample.js
+++ private_sample.js
@@ -1,3 +1,4 @@
 module.exports={
-    baseLogLocation: "/home/your_username/Documents/polymorph_logs"
+    baseLogLocation: "/home/your_username/Documents/polymorph_logs",
+    baseGitLocation: "/home/your_username/Documents/polymorph_gits"
 }
