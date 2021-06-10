b35cbd4611c9f6202012e91e4919da322eaf6598
acenturyandabit <steeven.liu2@gmail.com>
2021-06-10 21:02:34 +1000

    -

--- hilagit.js
+++ hilagit.js
@@ -510,6 +510,7 @@ function FileManager(docID, basepath) {
                 let inflatedHeadCommit = this.getInflatedCommit(headCommit);
                 let inflatedRemoteCommit = this.getInflatedCommit(remoteCommit);
                 // compare the commits
+                console.log(`headcommit had n keys: ${Object.keys(inflatedHeadCommit[i].items)}`);
                 let mutableCopyLatestCommit = {
                     source: thisServerIdentifier,
                     timestamp: Date.now(),
